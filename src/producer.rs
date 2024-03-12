11407bf4eab433fcc039e3d884acdc47c5746d28
Devon McKee <36770835+boingboomtschak@users.noreply.github.com>
2024-03-12 11:20:23 -0700

    Moving from paho to rumqttc, seems simpler

--- src/producer.rs
+++ src/producer.rs
@@ -1,7 +1,6 @@
 #![allow(unused_imports)]
 use std::process;
 use log::{LevelFilter, debug, error, info, trace, warn};
-use paho_mqtt as mqtt;
 
 fn main() {
     env_logger::builder().filter_level(LevelFilter::max()).init();
