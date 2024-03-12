0d91dba67923c813263272d76d05a916f7b20865
Devon McKee <36770835+boingboomtschak@users.noreply.github.com>
2024-03-12 10:46:33 -0700

    Adding initial setup for producer/consumer

---
+++ src/producer.rs
@@ -0,0 +1,10 @@
+#![allow(unused_imports)]
+use std::process;
+use log::{LevelFilter, debug, error, info, trace, warn};
+use paho_mqtt as mqtt;
+
+fn main() {
+    env_logger::builder().filter_level(LevelFilter::max()).init();
+
+    info!("starting producer!");
+}
