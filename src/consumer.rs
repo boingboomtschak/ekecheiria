e403f99580d049aac19b938f07761ae815007c1a
Devon McKee <36770835+boingboomtschak@users.noreply.github.com>
2024-03-13 15:36:03 -0700

    Adding image, uuid, working more on producer/consumer

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,9 +1,29 @@
 #![allow(unused_imports)]
 use std::process;
 use log::{LevelFilter, debug, error, info, trace, warn};
+use uuid::{Uuid};
+use rumqttc::{Client, LastWill, MqttOptions, QoS};
 
 fn main() {
     env_logger::builder().filter_level(LevelFilter::max()).init();
+    let id = "consumer:".to_owned() + &Uuid::new_v4().to_string();
+    info!("Starting consumer with id '{}'", id);
 
-    info!("starting consumer!");
+    let mut mqttoptions = MqttOptions::new(id, "localhost", 1883);
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+
+    client.subscribe("ekc-send", QoS::AtLeastOnce);
+
+    for (i, notification) in connection.iter().enumerate() {
+        match notification {
+            Ok(notif) => {
+                info!("{i}. Notification = {notif:?}");
+            }
+            Err(error) => {
+                error!("{i}. Notification = {error:?}");
+                return;
+            }
+        }
+    }
+    info!("Ending consumer...");
 }
--- src/producer.rs
+++ src/producer.rs
@@ -1,9 +1,50 @@
 #![allow(unused_imports)]
 use std::process;
 use log::{LevelFilter, debug, error, info, trace, warn};
+use uuid::{Uuid};
+use rumqttc::{Client, LastWill, MqttOptions, QoS};
+
+const IMAGES_PATH : &str = "images/";
+const PROCESSED_PATH : &str = "processed_images/";
+
+const SEND_TOPIC : &str = "ekc-send";
+const RECV_TOPIC : &str = "ekc-recv";
+const REG_TOPIC : &str = "ekc-reg";
+
+/*
+- producer loads job shader from file
+- producer publishes (synchronously) to send topic with job shader as payload
+- consumer(s) see publish, load job shader, set up pipeline, then publish to reg topic with their id and status
+- producer asynchronously reads status msgs from reg topic, saves list of ready 
+- producer reads list of images from folder, then for each:
+    - loads image as bytes, converts to necessary format
+    - finds next ready consumer in registered consumers
+    - publishes image as payload to send topic for consumer (likely send topic with uuid appended)
+    - marks them as processing
+    - asynchronously waits for recv back from consumer, which includes processed image as payload
+    - marks consumer as ready again
+*/
 
 fn main() {
     env_logger::builder().filter_level(LevelFilter::max()).init();
+    let id = "producer:".to_owned() + &Uuid::new_v4().to_string();
+    info!("Starting producer with id '{}'", id);
 
-    info!("starting producer!");
+    let mut mqttoptions = MqttOptions::new(id, "localhost", 1883);
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+
+    client.publish(SEND_TOPIC, QoS::AtLeastOnce, false, vec![]).unwrap();
+
+    for (i, notification) in connection.iter().enumerate() {
+        match notification {
+            Ok(notif) => {
+                info!("{i}. Notification = {notif:?}");
+            }
+            Err(error) => {
+                error!("{i}. Notification = {error:?}");
+                return;
+            }
+        }
+    }
+    info!("Ending producer...");
 }
