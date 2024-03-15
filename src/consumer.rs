454cbc836c48e43227e7ebbc5227243cfcc646ce
Devon McKee <36770835+boingboomtschak@users.noreply.github.com>
2024-03-15 14:49:30 -0700

    Starting to do event handling, setting up wgpu, adding some crates to help

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,29 +1,76 @@
 #![allow(unused_imports)]
-use std::process;
+use std::{process, fs};
 use log::{LevelFilter, debug, error, info, trace, warn};
 use uuid::{Uuid};
-use rumqttc::{Client, LastWill, MqttOptions, QoS};
+use rumqttc::{Client, LastWill, MqttOptions, QoS, Event, Incoming, Outgoing};
+use clap::Parser;
+use pollster::FutureExt;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    debug : bool
+}
+
 
 fn main() {
-    env_logger::builder().filter_level(LevelFilter::max()).init();
-    let id = "consumer:".to_owned() + &Uuid::new_v4().to_string();
+    let args = Args::parse(); 
+    env_logger::builder().filter_level(if args.debug { LevelFilter::Debug } else { LevelFilter::Info }).init();
+    debug!("CLI Args : {args:?}");
+
+    let id = &Uuid::new_v4().to_string();
     info!("Starting consumer with id '{}'", id);
 
     let mqttoptions = MqttOptions::new(id, "localhost", 1883);
     let (mut client, mut connection) = Client::new(mqttoptions, 10);
 
-    client.subscribe("ekc-send", QoS::AtLeastOnce);
+    let instance = wgpu::Instance::default();
+    let adapter = instance.request_adapter(
+        &wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: None
+        })
+        .block_on().expect("Failed to request adapter from instance");
+    let (device, queue) = adapter
+        .request_device(&Default::default(), None)
+        .block_on().expect("Failed to receive device from adapter");
 
-    for (i, notification) in connection.iter().enumerate() {
+    client.subscribe("ekc-init", QoS::AtLeastOnce).unwrap();
+    client.subscribe("ekc-send-".to_owned() + id, QoS::AtLeastOnce).unwrap();
+
+    for notification in connection.iter() {
         match notification {
-            Ok(notif) => {
-                info!("{i}. Notification = {notif:?}");
-            }
-            Err(error) => {
-                error!("{i}. Notification = {error:?}");
-                return;
+            Ok(evt_dir) => match evt_dir {
+                Event::Incoming(evt) => {
+                    debug!("MQTT< {evt:?}");
+                    if let Incoming::Publish(packet) = evt {
+                        match packet.topic.as_str() {
+                            "ekc-init" => {
+                                init_pipeline(String::from_utf8(packet.payload.to_vec()).expect("Error reading shader from init event!"));
+                                client.publish("ekc-reg", QoS::AtLeastOnce, false, id.as_bytes());
+                            },
+                            _ => ()
+                        }
+                    }
+                },
+                Event::Outgoing(evt) => {
+                    debug!("MQTT> {evt:?}");
+                }
+            },
+            Err(evt) => {
+                error!("MQTT! {evt:?}");
             }
         }
     }
     info!("Ending consumer...");
+}
+
+fn init_pipeline(shader : String) {
+    debug!("Shader: {shader}");
+}
+
+fn process_image() {
+
 }
--- src/producer.rs
+++ src/producer.rs
@@ -1,23 +1,33 @@
 #![allow(unused_imports)]
-use std::process;
+use std::{process, fs};
+use std::collections::HashMap;
 use log::{LevelFilter, debug, error, info, trace, warn};
 use uuid::{Uuid};
-use rumqttc::{Client, LastWill, MqttOptions, QoS};
+use rumqttc::{Client, LastWill, MqttOptions, QoS, Event, Incoming, Outgoing};
 use clap::Parser;
 
 const IMAGES_PATH : &str = "images/";
 const PROCESSED_PATH : &str = "processed_images/";
 
 #[derive(Parser, Debug)]
-#[command(name = "ekecheiria-producer")]
+#[command(version, about, long_about=None)]
 struct Args {
     #[arg()]
-    shader : String
+    shader : String,
+
+    #[arg(short, long)]
+    debug : bool
+}
+
+#[derive(Debug)]
+enum ConsumerStatus {
+    Ready,
+    Processing
 }
 
 /*
-- producer loads job shader from file
-- producer publishes (synchronously) to send topic with job shader as payload
+- producer loads job shader from file (*)
+- producer publishes (synchronously) to init topic with job shader as payload
 - consumer(s) see publish, load job shader, set up pipeline, then publish to reg topic with their id and status
 - producer asynchronously reads status msgs from reg topic, saves list of ready 
 - producer reads list of images from folder, then for each:
@@ -30,9 +40,12 @@ struct Args {
 */
 
 fn main() {
-    env_logger::builder().filter_level(LevelFilter::max()).init();
     let args = Args::parse(); 
-    debug!("args : {args:?}");
+    env_logger::builder().filter_level(if args.debug { LevelFilter::Debug } else { LevelFilter::Info }).init();
+    debug!("CLI Args : {args:?}");
+
+    info!("Loading shader '{0}'...", args.shader);
+    let shader = fs::read_to_string(args.shader).expect("Bad path to shader file");
     
     let id = "producer:".to_owned() + &Uuid::new_v4().to_string();
     info!("Starting producer with id '{}'", id);
@@ -40,19 +53,33 @@ fn main() {
     let mqttoptions = MqttOptions::new(id, "localhost", 1883);
     let (mut client, mut connection) = Client::new(mqttoptions, 10);
 
-    client.publish("ekc-send", QoS::AtLeastOnce, false, vec![]).unwrap();
+    client.publish("ekc-init", QoS::AtLeastOnce, false, shader.as_bytes()).unwrap();
     client.subscribe("ekc-reg", QoS::AtLeastOnce).unwrap();
     client.subscribe("ekc-recv", QoS::AtLeastOnce).unwrap();
 
+    let mut consumers : HashMap<String, ConsumerStatus> = HashMap::new();
 
-    for (i, notification) in connection.iter().enumerate() {
+    for notification in connection.iter() {
         match notification {
-            Ok(notif) => {
-                info!("{i}. Notification = {notif:?}");
-            }
-            Err(error) => {
-                error!("{i}. Notification = {error:?}");
-                return;
+            Ok(evt_dir) => match evt_dir {
+                Event::Incoming(evt) => {
+                    debug!("MQTT< {evt:?}");
+                    if let Incoming::Publish(packet) = evt {
+                        match packet.topic.as_str() {
+                            "ekc-reg" => {
+                                consumers.insert(String::from_utf8(packet.payload.to_vec()).expect("Error parsing consumer id from reg"), ConsumerStatus::Ready);
+                                debug!("Consumers: {consumers:?}");
+                            },
+                            _ => ()
+                        }
+                    }
+                },
+                Event::Outgoing(evt) => {
+                    debug!("MQTT> {evt:?}");
+                }
+            },
+            Err(evt) => {
+                error!("MQTT! {evt:?}");
             }
         }
     }
